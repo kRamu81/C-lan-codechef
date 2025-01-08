46235f47c182655823f5f02c097a91c49e7db381
kRamu581 <kanamramu18@gmail.com>
2025-01-08 21:19:15 +0530

    Update fiboncci series.c

--- fiboncci series.c
+++ fiboncci series.c
@@ -3,18 +3,17 @@
 int main() {
     int N, prev = 0, current = 1;
     scanf("%d", &N);
-    scanf("%d%d",prev,current);
-    
-    // Update your code below this line
-    for(int i=3;i<=N;i++)
-    {
-        int next = prev+current;
-        printf("%d",next);
+
+    // Print the first two numbers of the Fibonacci Series
+    printf("%d %d ", prev, current);
+
+    // Calculate and print the rest of the Fibonacci Series
+    for (int i = 3; i <= N; i++) {
+        int next = prev + current;
+        printf("%d ", next);
         prev = current;
         current = next;
-        
     }
-
 
     return 0;
 }
