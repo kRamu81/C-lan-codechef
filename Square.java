ab15d31551d3738bbf0820ed58d46208300ea058
kRamu81 <kanamramu18@gmail.com>
2025-01-25 07:23:32 +0530

    change

---
+++ Square.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Square{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a value:");
+        int A =sc.nextInt();
+        int s=A*A;
+        System.out.println(s);
+    }
+}
