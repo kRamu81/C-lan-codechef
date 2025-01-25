4b41f0e7345afefdf8df7e1568fc7402d6a92ec2
kRamu81 <kanamramu18@gmail.com>
2025-01-25 07:15:28 +0530

    change

---
+++ square.java
@@ -0,0 +1,11 @@
+import java.util.scanner;
+public class Square{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the side value":);
+        int side =sc.nextInt();
+        int sq=side *side;
+        System.out.println(sq);
+    }
+}
